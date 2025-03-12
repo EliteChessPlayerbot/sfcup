83a9725b1d19515b15027d7d118757bb58c9c20a
Veer Kumar Malik Singh <79562112+EliteChessPlayerbot@users.noreply.github.com>
2025-03-12 13:49:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+
+const auth = getAuth();
+
+function showSignup() {
+    document.getElementById("signup-form").style.display = "block";
+}
+
+function signup() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Signup Successful!");
+            document.getElementById("signup-form").style.display = "none";
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+}
+
+
 function updateProbability() {
     let veerWins = Math.floor(Math.random() * 100);
     let aryanWins = 100 - veerWins;
